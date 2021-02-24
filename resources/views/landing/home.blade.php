6e8d63f2c319c9c1c57e2b5b2533c52f60c59433
Gana Untaran <58590657+ganauntaran3@users.noreply.github.com>
2021-02-24 15:46:34 +0700

    Menambahkan card

--- resources/views/landing/home.blade.php
+++ resources/views/landing/home.blade.php
@@ -8,6 +8,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/7c32c656cb.js" crossorigin="anonymous"></script>
     <title>Document</title>
 </head>
@@ -24,21 +26,25 @@
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav mx-auto">
+                <ul class="navbar-nav mx-auto ">
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="#">Home</a>
                         <div class="underline"></div>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Features</a>
+                        <a class="nav-link" href="#">Products</a>
                         <div class="underline"></div>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Pricing</a>
+                        <a class="nav-link" href="#">Services</a>
                         <div class="underline"></div>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                        <a class="nav-link" href="#">About Us</a>
+                        <div class="underline"></div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact Us</a>
                         <div class="underline"></div>
                     </li>
                 </ul>
@@ -47,15 +53,129 @@
       </nav>
 
       <div class="header-banner">
+        <div class="container">
+            <div class="text-banner">
+                <h2 class=" poppins">Welcome to Bali Asri</h2>
+                <p>Find your taste of fashion and build your style here</p>
+                <button class="btn btn-discover">Discover now </button>
+            </div>
+
+
+        </div>
+      </div>
+
+      <div class="container card-negative">
+        <div class="row rounded-lg ">
+            <div class="col-lg-4">
+                <div class="card">
+                <div class="row ">
+                    <div class="col-lg-6">
+                        <img src="https://mdbootstrap.com/img/illustrations/graphics(4).png" class="img-fluid" alt="graphics">
+                        </div>
+                        <div class="col-lg-6 mb-4  ">
+                            <h1 class="mb-4">Title</h1>
+                            <p class="mb-4 ">Content</p>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="card">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <img src="https://mdbootstrap.com/img/illustrations/graphics(4).png" class="img-fluid" alt="graphics">
+                        </div>
+                        <div class="col-lg-6 mb-4  ">
+                            <h1 class="mb-4">Title</h1>
+                            <p class=" ">Content</p>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="card">
+                    <div class="row ">
+                        <div class="col-lg-6">
+                            <img src="https://mdbootstrap.com/img/illustrations/graphics(4).png" class="img-fluid" alt="graphics">
+                        </div>
+                        <div class="col-lg-6 mb-4  ">
+                            <h1 class="mb-4">Title</h1>
+                            <p class="mb-4 ">Content</p>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
 
       </div>
 
-      <div class="container">
-          <div class="row">
+      <div class="container-fluid">
+          <div class="row mt-3">
+            <div class="col-lg-3">
+                <div class="card">
+                    <div class="row shadow-none ">
+                        <div class="col-lg-6">
+                            <img src="https://mdbootstrap.com/img/illustrations/graphics(4).png" class="img-fluid" alt="graphics">
+                        </div>
+                        <div class="col-lg-6 mb-4  ">
+                            <h1 class="mb-4">Title</h1>
+                            <p class="mb-4 ">Content</p>
 
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-3">
+                <div class="card">
+                    <div class="row shadow-none ">
+                        <div class="col-lg-6">
+                            <img src="https://mdbootstrap.com/img/illustrations/graphics(4).png" class="img-fluid" alt="graphics">
+                        </div>
+                        <div class="col-lg-6 mb-4  ">
+                            <h1 class="mb-4">Title</h1>
+                            <p class="mb-4 ">Content</p>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-3">
+                <div class="card">
+                    <div class="row shadow-none ">
+                        <div class="col-lg-6">
+                            <img src="https://mdbootstrap.com/img/illustrations/graphics(4).png" class="img-fluid" alt="graphics">
+                        </div>
+                        <div class="col-lg-6 mb-4  ">
+                            <h1 class="mb-4">Title</h1>
+                            <p class="mb-4 ">Content</p>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-3">
+                <div class="card">
+                    <div class="row shadow-none ">
+                        <div class="col-lg-6">
+                            <img src="https://mdbootstrap.com/img/illustrations/graphics(4).png" class="img-fluid" alt="graphics">
+                        </div>
+                        <div class="col-lg-6 mb-4  ">
+                            <h1 class="mb-4">Title</h1>
+                            <p class="mb-4 ">Content</p>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
           </div>
       </div>
-
 
 
 
