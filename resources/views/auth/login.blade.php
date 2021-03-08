cbaf530b9e8964d96b449c712398199e87b78013
Gana Untaran <58590657+ganauntaran3@users.noreply.github.com>
2021-03-08 21:03:34 +0800

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -31,23 +31,8 @@
           <div class="row justify-content-center">
             <div class="col-lg-5 col-md-7">
               <div class="card bg-secondary border-0 mb-0">
-                <div class="card-header bg-transparent pb-5">
-                  <div class="text-muted text-center mt-2 mb-3"><small>Sign in with</small></div>
-                  <div class="btn-wrapper text-center">
-                    <a href="#" class="btn btn-neutral btn-icon">
-                      <span class="btn-inner--icon"><img src="../../assets/img/icons/common/github.svg"></span>
-                      <span class="btn-inner--text">Github</span>
-                    </a>
-                    <a href="#" class="btn btn-neutral btn-icon">
-                      <span class="btn-inner--icon"><img src="../../assets/img/icons/common/google.svg"></span>
-                      <span class="btn-inner--text">Google</span>
-                    </a>
-                  </div>
-                </div>
                 <div class="card-body px-lg-5 py-lg-5">
-                  <div class="text-center text-muted mb-4">
-                    <small>Or sign in with credentials</small>
-                  </div>
+
                   <form role="form" method="POST" action="{{ route('authenticate') }}">
                       @csrf
                     <div class="form-group mb-3">
@@ -78,47 +63,12 @@
                   </form>
                 </div>
               </div>
-              <div class="row mt-3">
-                <div class="col-6">
-                  <a href="#" class="text-light"><small>Forgot password?</small></a>
-                </div>
-                <div class="col-6 text-right">
-                  <a href="#" class="text-light"><small>Create new account</small></a>
-                </div>
-              </div>
+
             </div>
           </div>
         </div>
       </div>
 
-      <!-- Footer -->
-      <footer class="py-5" id="footer-main">
-        <div class="container">
-          <div class="row align-items-center justify-content-xl-between">
-            <div class="col-xl-6">
-              <div class="copyright text-center text-xl-left text-muted">
-                &copy; 2019 <a href="https://www.creative-tim.com" class="font-weight-bold ml-1" target="_blank">Creative Tim</a>
-              </div>
-            </div>
-            <div class="col-xl-6">
-              <ul class="nav nav-footer justify-content-center justify-content-xl-end">
-                <li class="nav-item">
-                  <a href="https://www.creative-tim.com" class="nav-link" target="_blank">Creative Tim</a>
-                </li>
-                <li class="nav-item">
-                  <a href="https://www.creative-tim.com/presentation" class="nav-link" target="_blank">About Us</a>
-                </li>
-                <li class="nav-item">
-                  <a href="http://blog.creative-tim.com" class="nav-link" target="_blank">Blog</a>
-                </li>
-                <li class="nav-item">
-                  <a href="https://www.creative-tim.com/license" class="nav-link" target="_blank">License</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </footer>
       <!-- Argon Scripts -->
       <!-- Core -->
       <script src="../../assets/vendor/jquery/dist/jquery.min.js"></script>
